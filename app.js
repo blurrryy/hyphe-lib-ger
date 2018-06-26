e186076fd8256b203442ffcf49f99f7d39e792ba
Daniel Rienas <blurrryy@gmail.com>
2018-06-26 17:04:27 +0200

    add zero-width space after slashes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const hyphe = require("./lib/hyphe-lib-ger");
 
-console.log(hyphe("Alle meine Entchön,\nschwimmen auf dem See!"));
+console.log(hyphe("Alle meine Entchön,\nschwimmen auf dem See/Teich!"));
 console.log("\nUnicode Output:");
 console.log(
-  hyphe("Alle meine Entchön,\nschwimmen auf dem See!", {
+  hyphe("Alle meine Entchön,\nschwimmen auf dem See/Teich!", {
     mode: "uni",
     ignoreLineBreak: true
   })
